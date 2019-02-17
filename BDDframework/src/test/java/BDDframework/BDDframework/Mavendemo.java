c670df44c7362690d9d187de635d1b2c17f77a10
SIVARAM <SIVARAM@192.168.0.4>
2019-02-17 22:42:09 +0530

    first Commit

---
+++ BDDframework/src/test/java/BDDframework/BDDframework/Mavendemo.java
@@ -0,0 +1,26 @@
+package BDDframework.BDDframework;
+
+import org.testng.annotations.Test;
+
+public class Mavendemo {
+	
+	@Test
+	
+	public void sum()
+	{
+		
+		System.out.println("SUM");
+		
+	}
+	
+	@Test
+	
+	public void Print()
+	{
+		
+		System.out.println("Sivaram");
+		
+	}
+	
+
+}
---
+++ BDDframework/src/test/java/BDDframework/BDDframework/Mavendemo1.java
@@ -0,0 +1,24 @@
+package BDDframework.BDDframework;
+
+import org.testng.annotations.Test;
+
+public class Mavendemo1 {
+
+@Test
+	
+	public void sum()
+	{
+		
+		System.out.println("SUM");
+		
+	}
+	
+	@Test
+	
+	public void Print()
+	{
+		
+		System.out.println("Sivaram");
+		
+	}
+}
